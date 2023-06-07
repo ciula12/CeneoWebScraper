9a821dc4bfe8c3c34860d210ddb1d1bcb1b8d0a3
ciula12 <ciula.wawrzyniec@gmail.com>
2023-06-07 12:59:03 +0200

    2023-06-07 WC

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,24 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Katarzyna Wójcik!"
+    return render_template('index.html')
 
-@app.route('/name/',defaults={'name': "Anonim"})
-@app.route('/name/<name>')
-def name(name):
-    return f"Hello, {name}!"
+@app.route('/extract')
+def extract():
+    return render_template('extract.html')
+
+@app.route('/products')
+def products():
+    return render_template('products_list.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+    
+@app.route('/author')
+def author():
+    return render_template('author.html')
